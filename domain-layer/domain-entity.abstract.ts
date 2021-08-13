e44c7cbd8d38c643aa064350c508ba773e41e294
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-08-13 20:55:51 +0000

    Remove created and updated date from DomainEntity

--- domain-layer/domain-entity.abstract.ts
+++ domain-layer/domain-entity.abstract.ts
@@ -3,19 +3,11 @@ import { IIdentifier } from './interfaces/identifier.interface';
 
 export abstract class DomainEntity<T extends BaseProps> implements IEntity {
   public readonly _id: IIdentifier;
-  public readonly _createdAt: Date;
-  public  _updatedAt: Date;
   protected readonly props: T;
 
   protected constructor(props: T, id: IIdentifier) {
     this._id = id;
-    this._createdAt = new Date();
-    this._updatedAt = new Date();
     this.props = props;
-  }
-
-  protected update(){
-    this._updatedAt = new Date();
   }
 
   public equals(entity: DomainEntity<T>): boolean {
