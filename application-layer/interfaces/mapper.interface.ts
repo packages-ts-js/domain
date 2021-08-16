087347c4e4e861c3d97cb4420b55f5349f1aa631
danieloralndo97 <daniel.ortiz@estudiantes.matcom.uh.cu>
2021-08-15 23:38:45 -0700

    Add mapper interface

---
+++ application-layer/interfaces/mapper.interface.ts
@@ -0,0 +1,19 @@
+export interface IMapperPersistentDomain<Entity, Domain>{
+
+}
+
+export interface IMapperDomainCreate<Domain, CreateDto>{
+    
+}
+
+export interface IMapperDomainUpdate<Domain, UpdateDto>{
+    
+}
+
+export interface IMapperDomainSummary<Domain, SummaryDto>{
+    
+}
+
+export interface IMapperDomainDetatils<Domain, DetatilsDto>{
+    
+}
