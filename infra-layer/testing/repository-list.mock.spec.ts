e8727213957f9cdb20f0b8342b3fad6219753d0f
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-08-19 15:39:16 -0400

    Add test to save mock

--- infra-layer/testing/repository-list.mock.spec.ts
+++ infra-layer/testing/repository-list.mock.spec.ts
@@ -29,6 +29,12 @@ describe("MooringTypeServices", () => {
         expect(result).toStrictEqual([{id: id, name:name}])
 
     })
-    
 
+    it("save item",  async () => {
+        const id = "1"
+        const name = "TestMockRepo2"
+        const result = await repoMock.save( {id:id, name:name} )
+        expect(result.id).toBe(id)
+        expect(result.name).toBe(name)
+    })
 })
